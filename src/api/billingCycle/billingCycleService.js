126164689b504e0f4f477ccebc2d38d110e7120a
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-07-08 09:42:38 -0300

    Testando a API(Postman)

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,8 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+//new: Ao atualizar, retornar o objeto novo.
+//runValidators: para as validações funcionarem
 BillingCycle.updateOptions({new: true, runValidators: true})
 
 module.exports = BillingCycle
--- src/config/database.js
+++ src/config/database.js
@@ -2,3 +2,11 @@ const mongoose = require('mongoose')
 //Pegando a api de promise do node e atribuindo para a api de promise do mongoose
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH} é obrigatório."
+mongoose.Error.messages.Number.min = 
+"O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = 
+"O '{VALUE}' informado é maior que o limite maximo de '{MAX}'."
+mongoose.Error.messages.String.enum = 
+"'{VALUE}' não é valído para o atributo '{PATH}'."
